cf0fdcc2003953272634c4aac8324362865f1496
Katrina Uychaco <kuychaco@gmail.com>
2015-08-12 17:57:56 -0700

    Add basic portal for Part 2 curriculum

---
+++ app.js
@@ -0,0 +1,22 @@
+$('document').ready(function() {
+
+  // on click of li
+    // if no iframe is displayed below
+      //  insert embed iframe after li - set size dynamically (.width, set height to be 73% width)
+    //  else
+      //  toggle visibility of iframe
+
+  $('.slidesList').click(function(event) {
+    var target = event.target;
+    var next = $(target).next()[0];
+    if (next.localName !== 'iframe') {
+      var embedURL = $(target).data('embedurl');
+      $(target).after('<iframe src=' + embedURL + ' width="576" height="420" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+    } else {
+      $(next).toggle();
+    }
+  });
+
+
+
+});
