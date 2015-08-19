9d1308cc2a192141218ecfc45d793031d4f331b4
Katrina Uychaco <kuychaco@gmail.com>
2015-08-19 16:54:13 -0700

    Add portal page for Part 1

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ $('document').ready(function() {
   $('.slidesList, .pairing').click(function(event) {
     var target = event.target;
     var next = $(target).next()[0];
-    if (next.localName !== 'iframe') {
+    if ((next && next.localName !== 'iframe') || next === undefined) {
       var embedURL = $(target).data('embedurl');
       $(target).after('<iframe src=' + embedURL + ' width="576" height="420" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
     } else {
